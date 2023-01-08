02fdfd96501522acca2feed6710e8f67de77fb6d
Pipijopa <efimovivan02@mail.ru>
2023-01-08 19:48:31 +0400

    react

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { useCallback } from 'react';
+import Button from '../Button/Button';
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const gitButton = useCallback( 
+    () => {
+      window.open('https://github.com/Pipijopa', '_blank', 'noopener,noreferrer')
+    }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <h1 className={styles.logo}>Админка фильмотеки</h1>
+        <Button onClick={gitButton}>Ефимов Иван</Button>
       </div>
     </header>
   );
--- src/components/MovieComponents/MovieAside/MovieCard/MovieCard.tsx
+++ src/components/MovieComponents/MovieAside/MovieCard/MovieCard.tsx
@@ -1,5 +1,5 @@
 import React, {memo} from 'react';
-import {MovieView} from '../../../types/movie';
+import {MovieView} from '../../../../types/movie';
 import styles from './MovieCard.module.scss';
 import cn from 'classnames';
 
@@ -10,7 +10,8 @@ export const MovieCard: React.FC<{card: MovieView; active: boolean}> = memo(({ca
     <article className={cn(styles.card, {[styles.card_selected]: active})}>
       <h3 className={styles.card__title}>{title}</h3>
       <div className={styles.card__info}>
-        <span>{year}</span>
+        <span className={styles.card__info__year}> {year}</span>
+        <span className={styles.card__info__separator}>|</span>
         <ul>
           {genres.map((item) => (
             <li key={item}>{item}</li>
